e3f541e6c54a28ee4d6cb9a771e950661fff6bc5
nottdev <siriphon@gmail.com>
2020-05-10 18:01:41 +0700

    Add express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname+'/index.html')
+})
+
+app.listen(port, ()=>console.log(`Server is running on port ${port}`))
